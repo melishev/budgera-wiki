218b0185b7befe21ff9209a312f719a3c5c313b3
Matvey Melishev <melishevm@gmail.com>
2026-01-09 01:41:44 +0100

    fix: update query to use 'docs' collection and remove obsolete content configuration

--- components/DocsPageHeaderLinks.vue
+++ components/DocsPageHeaderLinks.vue
@@ -2,7 +2,7 @@
 const route = useRoute()
 
 const { data: page } = await useAsyncData(route.path, () => {
-  return queryCollection('content').path(route.path).first()
+  return queryCollection('docs').path(route.path).first()
 })
 </script>
 
